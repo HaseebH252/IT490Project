2c015e0c6c0b6530e845992227c0a7caad03fbe8
Pavel Orlyukov <po42@njit.edu>
2019-10-26 18:12:42 -0400

    cleaned up attom api

--- api_calls/attom.php
+++ api_calls/attom.php
@@ -1,32 +1,24 @@
 <?php
 
 //receives each found property and retrieves only required fields
-
 function splitObject($property){
-    //$street = $response->property[0]->address->oneLine."\n";
+
     $street = $property->address->oneLine;
     $latitude = $property->location->latitude;
     $longitude = $property->location->longitude;
+    $lineOne = $property->address->line1;
+    $lineTwo = $property->address->line2;
 
     //adds all required variables into associative array
-    $info = array('street' => $street,'latitude' => $latitude,'longitude' => $longitude);
+    $info = array('street' => $street,'latitude' => $latitude,'longitude' => $longitude,'line1' => $lineOne,'line2'=>$lineTwo);
     return $info;
 }
 
-//First function, Currently receives only zipcode for testing purposes
-//Makes call to api, receives a json string
-//string then decoded into array
-function receiveCurlAttom($zip){
+//performs api call
+function attomApiCall($postalCode,$pagesize){
     $curl = curl_init();
-    //$postalCode = $message;
-    $postalCode = $zip;
-
-    //pagesize regulates how many search results we get back from api
-    $pagesize= "4";
-
-    //curl code from tutorial
     curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=".$postalCode."&page=1&pagesize=".$pagesize."",
+        CURLOPT_URL => "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=".$postalCode."&propertytype=APARTMENT&page=1&pagesize=".$pagesize."",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
@@ -38,46 +30,51 @@ function receiveCurlAttom($zip){
             "apikey: d85d17849905c3125d52318e0898e80b",
         ),
     ));
+
     //this variable receives data returned from api
     $response = curl_exec($curl);
     $err = curl_error($curl);
     curl_close($curl);
 
-    //json string is decoded
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+
+    }
+
     $response = json_decode($response);
+    return $response;
+}
 
-    //declaring array that will be returned from this function
-    //this array will be nested in the array that will be returned to front end
-    $valuesArray = array();
 
-    //loops through each property from api call
-    //this array will be nested
-    //it will contain arrays for each found property, but only with data that we actually require
-    //calls function splitObject()
-    foreach ($response->property as $row){
-        $valuesArray[] = splitObject($row);
+//Main Function for attom.php, receives Zipcode as parameter
+function receiveCurl($zip){
+
+    //declaring array that will be returned from this function, will return to from this function
+    $addresses = array();
+    $postalCode = $zip;
+
+    //pagesize regulates how many search results we get back from api
+    $pagesize= "5";
+
+    //calls attom API call function to retrieve Addresses as one line, coordinates, and addresses in two line format (needed for attomBasicProfile())
+    $justAddresses = attomApiCall($postalCode,$pagesize);
+
+    //loops through each property from api call, calls function splitObject()
+    foreach ($justAddresses->property as $row){
+        $addresses[] = splitObject($row);
     }
 
     //debug
-    //echo print_r($valuesArray);
+    echo print_r($addresses);
     //get address as a line
     //$street = $response->property[0]->address->oneLine."\n";
     //echo print_r($street);
     //echo gettype($response)."\n";
     //echo print_r($response)."\n";
 
-    if ($err) {
-        echo "cURL Error #:" . $err;
-    } else {
-        //echo $response."\n";
-        //return $valuesArray;
-    }
-    return $valuesArray;
+    return $addresses;
 }
 //TO TEST THIS CODE
-
-
-$attomAddresses= receiveCurlAttom('07108');
-
-//print_r($attomAddresses);
+receiveCurl('07108');
 ?>
