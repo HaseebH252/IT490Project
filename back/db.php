73c753f9b7f4981d658364fc48fb7ada6d2075b1
hh262 <36900889+hh262@users.noreply.github.com>
2019-12-15 14:03:27 -0500

    12/15

--- back/db.php
+++ back/db.php
@@ -4,6 +4,10 @@ $sql_host = '192.168.1.101';
 $sql_user = 'test';
 $sql_pass = 'Today123$';
 $sql_db = 'Users';
+
+$backup_host = '192.168.1.110';
+
+
 $mysqli = new mysqli($sql_host,$sql_user,$sql_pass,$sql_db) or die($mysqli->error);
 
 ?>
